bfd731befc3e4daf18e0754ea06e617e034c38fd
Artyom <csgo1320@gmail.com>
2022-08-10 20:22:15 +0300

    .

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Writeline("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Writeline("Введите второе число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
