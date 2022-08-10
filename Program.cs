9b4b4c5f3e57204151a08e48ba295399557fd341
Artyom <csgo1320@gmail.com>
2022-08-10 20:42:11 +0300

    аписали программу для решения задачи

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿Console.Writeline("Введите первое число: ");
+﻿Console.WriteLine("Введите первое число: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Writeline("Введите второе число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
 
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("max " + firstNumber);
+}
+else Console.WriteLine("max " + secondNumber);
